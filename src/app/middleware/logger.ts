119c9370307e9bcbec098184c240ef5687ffba0d
Adrian Rossing <adriangrossing@gmail.com>
2018-06-16 15:40:42 -0500

    changes to names

--- src/app/middleware/logger.ts
+++ src/app/middleware/logger.ts
@@ -1,6 +1,9 @@
 import { Middleware } from 'redux';
 
 export const logger: Middleware = (store) => (next) => (action) => {
-  console.log(action);
-  return next(action);
+  console.log('state before: ',store.getState());
+  console.log('action: ',action);
+  next(action);
+  console.log('state after: ',store.getState());
+  return; 
 };
